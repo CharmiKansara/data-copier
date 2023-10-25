ff62ccf144675367c5d653f31ee2c76115fc0d8a
Charmi Kansara <charmi18kansara@gmail.com>
2023-10-24 23:36:26 -0400

    added logic tovread table list

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
 import sys
 from config import DB_DETAILS
-
+from util import get_tables
 def main():
+
+    
     #program takes atleast one argument
     env = sys.argv[1]
     db_details = DB_DETAILS[env]
-    print(db_details)
-    for args in sys.argv:
-        print(args)
-    print("hello")
+    tables = get_tables('table_list.txt')
+    for table in tables['table_name']:
+        print(table)
+   
+    
+    
 
 if __name__=="__main__":
     main()  
